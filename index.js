762f626998f3b150cbb62ba8fec9d5eb5813482b
Mark Robertson <markrivasrobertson@yahoo.com>
2017-06-29 15:31:52 -0400

    add functionality to record data from input elements

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const messageLog = [];
 class Player {
   constructor(num) {
     this.playerNumber = num;
-    console.log('player constructor');
+    // console.log('player constructor');
   }
   sendMessage(rec, txt) {
     const newMessage = new Message(this.playerNumber, rec, txt);
@@ -25,7 +25,20 @@ function setup() {
   for (let i = 1; i < 5; i++) {
     const newPlayer = new Player(i);
     players.push(newPlayer);
-    console.log(newPlayer);
+    // console.log(newPlayer);
   }
 }
 setup();
+
+function handleMessage(num) {
+  // console.log(`handle message fired for player ${num}`);
+  const recipientSource = document.getElementById(`recipient${num}`);
+  const messageSource = document.getElementById(`text${num}`);
+  const recipient = parseInt(recipientSource.value);
+  const message = messageSource.value;
+  players[num-1].sendMessage(recipient, message);
+  messageSource.value = '';
+  console.log(messageSource.value);
+  recipientSource.value = '';
+  console.log(recipientSource.value);
+}
